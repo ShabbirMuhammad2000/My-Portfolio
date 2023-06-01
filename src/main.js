99007160edcbdc3720a84e107087fc158840219e
Mubi9000 <shabbirmuhammad2000@gmail.com>
2023-06-01 17:56:26 -0400

    Added lighting efffect

--- src/main.js
+++ src/main.js
@@ -23,6 +23,12 @@ document.addEventListener('DOMContentLoaded', () => {
   
   scene.add(torus)
 
+  const pointLight = new THREE.PointLight(0xffffff)
+  pointLight.position.set(10,10,10)
+  
+  const ambientLight = new THREE.AmbientLight(0xffffff)
+  scene.add(pointLight, ambientLight)
+
   function animate() {
     requestAnimationFrame(animate)
 
