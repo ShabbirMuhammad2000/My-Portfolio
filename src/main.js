d6b507afa4353b76d3f597311055228e525cffe6
Mubi9000 <shabbirmuhammad2000@gmail.com>
2023-06-01 21:19:07 -0400

    Added orbit controls and stars background

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { render } from 'vue'
 import './style.css'
 import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 
 document.addEventListener('DOMContentLoaded', () => {
   const scene = new THREE.Scene()
@@ -29,6 +30,25 @@ document.addEventListener('DOMContentLoaded', () => {
   const ambientLight = new THREE.AmbientLight(0xffffff)
   scene.add(pointLight, ambientLight)
 
+const lightHelper = new THREE.PointLightHelper(pointLight)
+const gridHelper = new THREE.GridHelper(200, 50)
+scene.add(lightHelper, gridHelper)
+
+const controls = new OrbitControls(camera, renderer.domElement)
+
+function addStar() {
+  const geometry = new THREE.SphereGeometry(0.25, 24, 24)
+  const material = new THREE.MeshStandardMaterial({color: 0xffffff})
+  const star = new THREE.Mesh(geometry, material)
+
+  const [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(100))
+  
+  star.position.set(x,y,z)
+  scene.add(star)
+}
+
+Array(200).fill().forEach(addStar)
+
   function animate() {
     requestAnimationFrame(animate)
 
@@ -36,8 +56,11 @@ document.addEventListener('DOMContentLoaded', () => {
     torus.rotation.y += 0.005
     torus.rotation.z += 0.01
 
+    controls.update()
+
     renderer.render(scene, camera)
   }
 
   animate()
 })
+
